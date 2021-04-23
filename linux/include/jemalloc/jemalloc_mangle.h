1f6161d0e3d17fb08e95fb8f4fc4cc51a402970f
Steven Noonan <steven@uplinklabs.net>
2021-04-23 09:01:13 -0700

    linux: reconfigure
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- linux/include/jemalloc/jemalloc_mangle.h
+++ linux/include/jemalloc/jemalloc_mangle.h
@@ -23,7 +23,7 @@
 #  define malloc_stats_print je_malloc_stats_print
 #  define malloc_usable_size je_malloc_usable_size
 #  define mallocx je_mallocx
-#  define smallocx_9031c232c6c35bb351c3deb8dbccc6c1cd756645 je_smallocx_9031c232c6c35bb351c3deb8dbccc6c1cd756645
+#  define smallocx_f98a6f930490f4ca288be9af3719de1305d9c827 je_smallocx_f98a6f930490f4ca288be9af3719de1305d9c827
 #  define nallocx je_nallocx
 #  define posix_memalign je_posix_memalign
 #  define rallocx je_rallocx
@@ -57,7 +57,7 @@
 #  undef je_malloc_stats_print
 #  undef je_malloc_usable_size
 #  undef je_mallocx
-#  undef je_smallocx_9031c232c6c35bb351c3deb8dbccc6c1cd756645
+#  undef je_smallocx_f98a6f930490f4ca288be9af3719de1305d9c827
 #  undef je_nallocx
 #  undef je_posix_memalign
 #  undef je_rallocx
