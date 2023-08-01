001cbfe5bca52a9ad5c34b1374829cd2d32995ea
Steven Noonan <steven@uplinklabs.net>
2023-08-01 02:05:35 -0700

    stats: silence clang 17 warning about unused variables
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- src/stats.c
+++ src/stats.c
@@ -13,6 +13,7 @@
 #include "jemalloc/internal/stats_internal.h"
 #include "jemalloc/internal/tcache.h"
 
+#ifdef JEMALLOC_STATS_PRINT
 static const char *const global_mutex_names[mutex_prof_num_global_mutexes] = {
 #define OP(mtx) #mtx,
     MUTEX_PROF_GLOBAL_MUTEXES
@@ -24,6 +25,7 @@ static const char *const arena_mutex_names[mutex_prof_num_arena_mutexes] = {
     MUTEX_PROF_ARENA_MUTEXES
 #undef OP
 };
+#endif
 
 /******************************************************************************/
 /* Data. */
